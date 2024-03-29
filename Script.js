9d3e32f42c9ca5c773545775b62f044885c70aa1
Lucca Gabriel <luccagabriel@MacBook-Air-de-Lucca.local>
2024-03-29 13:30:54 -0300

    Melhorando transação

--- Script.js
+++ Script.js
@@ -1,15 +1,37 @@
-let count = 1;
+let controlador = {
+    atual: 1,
+    anteriror: null
+}
+
 document.getElementById("radio1").checked = true;
+
+for (const radio of ["radio1", "radio2", "radio3", "radio4", "radio5", "radio6", "radio7", "radio8"]) {
+    const element = document.getElementById(radio)
+    element.onclick = () => {
+        controlador.anteriror = controlador.atual
+        controlador.atual = Number(radio.replace("radio", ""))
+        document.getElementById(`manual-radio${controlador.anteriror}`).style.backgroundColor = "transparent"
+        document.getElementById("manual-radio" + controlador.atual).style.backgroundColor = "#000000"
+    }
+}
+
 
 setInterval(function () {
     nextImage();
 }, 4000)
 
 function nextImage() {
-    count++;
-    if (count > 8) {
-        count = 1;
+    controlador.anteriror = controlador.atual;
+    controlador.atual++;
+    if (controlador.atual > 8) {
+        document.getElementById(`manual-radio${controlador.anteriror}`).style.backgroundColor = "transparent"
+        controlador.atual = 1;
     }
-    document.getElementById("radio" + count).checked = true;
+
+    if (controlador.atual > 1) {
+        document.getElementById(`manual-radio${controlador.anteriror}`).style.backgroundColor = "transparent"
+    }
+    document.getElementById("radio" + controlador.atual).checked = true;
+    document.getElementById("manual-radio" + controlador.atual).style.backgroundColor = "#000000"
 
 }
